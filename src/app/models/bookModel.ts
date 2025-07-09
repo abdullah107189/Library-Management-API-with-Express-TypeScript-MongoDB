59f0be7ef7f8255f6c1e5acdacb6c8846263f012
abdullah107189 <abdullah107189@gmail.com>
2025-07-09 23:12:14 +0600

    create book schema

---
+++ src/app/models/bookModel.ts
@@ -0,0 +1,29 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/bookInterface";
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: {
+    type: String,
+    required: true,
+    enum: [
+      "BIOGRAPHY",
+      "FANTASY",
+      "FICTION",
+      "HISTORY",
+      "NON_FICTION",
+      "SCIENCE",
+    ],
+  },
+  isbn: { type: String, required: true, unique: true },
+  description: String,
+  copies: {
+    type: Number,
+    min: [0, "Not allow negative number"],
+    required: true,
+  },
+  available: { type: Boolean, default: true },
+});
+
+export const Book = model("Books", bookSchema);
